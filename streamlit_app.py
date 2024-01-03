54e2b042d718831bba805784a3222e5274269021
MurthyAnyam <155423076+MurthyAnyam@users.noreply.github.com>
2024-01-03 16:32:42 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,3 @@
-import streamlit
-streamlit.title('My parents health new diner')
-streamlit.header('🍞Breakfast Menu')
-streamlit.text('🥑🍞Egg')
-streamlit.text('🥑🍞hasbrown')
-streamlit.text('🥣 🥗 Bread')
-
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
