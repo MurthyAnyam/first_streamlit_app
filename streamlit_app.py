5474239d61d65b67dcd70c7f4d8eba3101cbf98b
MurthyAnyam <155423076+MurthyAnyam@users.noreply.github.com>
2024-01-03 13:45:27 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('My parents health new diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Egg')
-streamlit.text('hasbrown')
-streamlit.text('Bread')
+streamlit.header('🍞Breakfast Menu')
+streamlit.text('🥑🍞Egg')
+streamlit.text('🥑🍞hasbrown')
+streamlit.text('🥣 🥗 Bread')
